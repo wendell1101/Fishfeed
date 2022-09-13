2e5546345ac6bf12c6c626321cf4fdb6504a814f
Rico Guinanao <83864808+coricss@users.noreply.github.com>
2022-09-13 19:17:19 +0800

    fish feeds calculation ui

--- resources/views/calculation.blade.php
+++ resources/views/calculation.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="calculation-wrapper">
       <div class="row gx-3 gy-3 w-100">
-        <div class="col-lg-6">
+        <div class="col-lg-4">
           <div class="bg-light rounded-2 p-3 h-100">
             <div class="round-type-calculation">
               <img src="{{asset('imgs/round-type-calculation.png')}}" class="rounded-2" width="100%" alt="fish" class="fish"/>
@@ -17,7 +17,7 @@
             </a>
           </div>
         </div>
-        <div class="col-lg-6">
+        <div class="col-lg-4">
           <div class="bg-light rounded-2 p-3 h-100">
             <div class="round-type-calculation">
               <img src="{{asset('imgs/rectangular-fish-ponds.png')}}" class="rounded-2" width="100%" alt="fish" class="fish"/>
@@ -30,7 +30,20 @@
             </a>
           </div>
         </div>
-         
+        <div class="col-lg-4">
+            <div class="bg-light rounded-2 p-3 h-100">
+              <div class="round-type-calculation">
+                <img src="{{asset('imgs/fish-feeds.png')}}" class="rounded-2" width="100%" alt="fish" class="fish"/>
+              </div>
+              <a href="{{route('fish_feeds')}}" class="text-decoration-none">
+                <button class="align-items-center d-flex justify-content-center btn btn-primary btn-lg mt-3 w-100" type="submit">
+                  <i class='bx bxs-calculator me-1'></i>
+                  <p class="m-0">Calculate feeds</p>
+                </button>
+              </a>
+            </div>
+          </div>
+
 
       </div>
 
